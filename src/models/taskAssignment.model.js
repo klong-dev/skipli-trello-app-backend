b0d2ba3d8a905bf22f1c3d0a1ed102a08448002b
Hoàng Kim Long <longhoang8204@gmail.com>
2025-08-19 00:41:57 +0700

    feat(Task): added task assignment model

---
+++ src/models/taskAssignment.model.js
@@ -0,0 +1,31 @@
+// filepath: c:\Users\user\Desktop\WebStorm\SkipliBoardManagement\src\models\taskAssignment.model.js
+const {DataTypes} = require('sequelize');
+const {sequelize} = require("../utils/db");
+const Task = require("./task.model");
+
+const TaskAssignment = sequelize.define("taskAssignment",
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        taskId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: Task,
+                key: 'id'
+            }
+        },
+        memberId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = TaskAssignment;
